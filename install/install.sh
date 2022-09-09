7f91651cd056033462241d6f64481d5f76e7d7eb
Gardner Berry <berrygw06@gmail.com>
2022-09-09 18:44:11 -0400

    fixed spelling and syntax

--- install/install.sh
+++ install/install.sh
@@ -3,22 +3,22 @@
 echo "Welcome to my Installation Script for Arch Linux and Arch Linux based distros"
 
 echo "installing awesomewm config"
-echo "cp -r ~/dotfiles/awesome ~/.config"
+cp -r ~/dotfiles/awesome ~/.config
 echo "installing .bashrc"
-echo "cp -r ~/dotfiles/bash/.bashrc ~/"
+cp -r ~/dotfiles/bash/.bashrc ~/
 echo "installing fish config"
-echo "cp -r ~/dotfiles/fish ~/.config"
+cp -r ~/dotfiles/fish ~/.config
 echo "installing alacritty config"
-echo "cp -r ~/dofiles/alacritty ~/.config"
+cp -r ~/dotfiles/alacritty ~/.config
 echo "installing doom emacs config"
-echo "cp -r ~/dotfiles/.doom.d ~/"
+cp -r ~/dotfiles/.doom.d ~/
 echo "installing .zshrc"
-echo "cp -r ~/dofiles/zsh/.zshrc ~/"
+cp -r ~/dotfiles/zsh/.zshrc ~/
 echo "installing my btop config"
-echo "mkdir ~/.config/btop&&cp -r ~/dotfiles/btop/btop.conf ~/.config/btop"
+mkdir ~/.config/btop&&cp -r ~/dotfiles/btop/btop.conf ~/.config/btop
 
 echo "updating"
-echo "sudo pacman -Syu"
+sudo pacman -Syu
 
 sleep 2
 
@@ -26,6 +26,6 @@ sleep 2
 #echo "pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si"
 #If it is than
 
-echo "yay -S awesome dmenu picom brave-bin librewolf-bin starship exa btop pfetch pokemon-colorscripts-git lxapperance emacs nemo alacritty zsh alacritty-themes lxappearance nitrogen fish neovim ripgrep fd chromium"
+yay -S awesome dmenu picom brave-bin librewolf-bin starship exa btop pfetch pokemon-colorscripts-git lxapperance emacs nemo alacritty zsh alacritty-themes lxappearance nitrogen fish neovim ripgrep fd chromium
 
 
