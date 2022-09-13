67e3c84d9d13aa011c443508985d96915f35c0b8
Gardner Berry <berrygw06@gmail.com>
2022-09-12 23:19:48 -0400

    added exa -lah flag to fish

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,3 +1,3 @@
-function ls --wraps='exa -al --color=always --group-directories-first' --description 'alias ls=exa -al --color=always --group-directories-first'
-  exa -al --color=always --group-directories-first $argv; 
+function ls --wraps='exa -al --color=always --group-directories-first' --wraps='exa -lah --color=always --group-directories-first' --description 'alias ls=exa -lah --color=always --group-directories-first'
+  exa -lah --color=always --group-directories-first $argv; 
 end
