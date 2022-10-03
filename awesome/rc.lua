ad94963e6d3ae1350d78ea431c4de94d0cdf6cd3
Gardner Berry <berrygw06@gmail.com>
2022-10-03 18:35:39 -0400

    added volumeicon to rc.lua

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -340,8 +340,8 @@ globalkeys = gears.table.join(
 --              {description = "launch librewolf", group = "applications"}),    
 
 -- Chromium 
-   awful.key({ modkey },            "c",     function () awful.util.spawn("chromium") end,
-             {description = "launch chromium", group = "applications"}),
+--   awful.key({ modkey },            "c",     function () awful.util.spawn("chromium") end,
+--             {description = "launch chromium", group = "applications"}),
 
 -- Virt-Manager
     awful.key({ modkey },            "v",     function () awful.util.spawn("virt-manager")end,
@@ -556,46 +556,6 @@ client.connect_signal("manage", function (c)
     end
 end)
 
--- Add a titlebar if titlebars_enabled is set to true in the rules.
---client.connect_signal("request::titlebars", function(c)
-    -- buttons for the titlebar
---    local buttons = gears.table.join(
---        awful.button({ }, 1, function()
---            c:emit_signal("request::activate", "titlebar", {raise = true})
---            awful.mouse.client.move(c)
---        end),
---        awful.button({ }, 3, function()
---            c:emit_signal("request::activate", "titlebar", {raise = true})
---            awful.mouse.client.resize(c)
---        end)
---    )
-
---    awful.titlebar(c) : setup {
---        { -- Left
---            awful.titlebar.widget.iconwidget(c),
---            buttons = buttons,
---            layout  = wibox.layout.fixed.horizontal
---        },
---        { -- Middle
---            { -- Title
---                align  = "center",
---                widget = awful.titlebar.widget.titlewidget(c)
---            },
---            buttons = buttons,
---            layout  = wibox.layout.flex.horizontal
---        },
---        { -- Right
---            awful.titlebar.widget.floatingbutton (c),
---            awful.titlebar.widget.maximizedbutton(c),
---            awful.titlebar.widget.stickybutton   (c),
---            awful.titlebar.widget.ontopbutton    (c),
---            awful.titlebar.widget.closebutton    (c),
---            layout = wibox.layout.fixed.horizontal()
---        },
---        layout = wibox.layout.align.horizontal
---    }
---end)
-
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", {raise = false})
@@ -612,9 +572,10 @@ awful.spawn("nm-applet")
 awful.spawn("/usr/bin/emacs --daemon")
 --awful.spawn("polybar")
 --awful.spawn("rustdesk")
+awful.spawn("volumeicon")
 
 --Window Gaps
-beautiful.useless_gap = 4
+beautiful.useless_gap = 8
 
 --Set up a Network Widget
 --local net_widgets = require("net_widgets")
