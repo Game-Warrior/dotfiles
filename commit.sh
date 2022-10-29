07936524afbd3939f46cf1e5c2122aac783ff847
Gardner Berry <berrygw06@gmail.com>
2022-10-29 11:39:50 -0400

    addtion(Added "delete"&"addtion" to commit.sh):

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-TYPE=$(gum choose "fix" "feat" "docs" "style" "refactor" "test" "chore" "revert")
+TYPE=$(gum choose "fix" "docs" "style" "refactor" "test" "chore" "revert" "delete" "addtion")
 SCOPE=$(gum input --placeholder "scope")
 
 # Since the scope is optional, wrap it in parentheses if it has a value.
