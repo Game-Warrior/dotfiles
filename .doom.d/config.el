5d7544e706ce4db52acd5e906159576178444014
Gardner Berry <berrygw06@gmail.com>
2022-10-30 18:57:11 -0400

    updated .doom.d

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,127 +1,30 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+> Tangling your literate config...
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+Tangled 46 code blocks from config.org
+  [032m✓ Done tangling 1 file(s)![0m
+> Tangling your literate config...
 
+Tangled 46 code blocks from config.org
+  [032m✓ Done tangling 1 file(s)![0m
+> Tangling your literate config...
 
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-(setq user-full-name "Gardner Berry"
-      user-mail-address "berrygw06@gmail.com")
+Debugger entered--Lisp error: (wrong-type-argument stringp nil)
+  regexp-quote(nil)
+  org-src-coderef-regexp(nil)
+  org-babel-tangle-single-block(1)
+  org-babel-tangle-collect-blocks(nil nil)
+  org-babel-tangle(nil "/home/gardner/.doom.d/config.el" nil)
+  org-babel-tangle-file("/home/gardner/.doom.d/config.org" "/home/gardner/.doom.d/config.el")
+  (and t (org-babel-tangle-file target dest))
+  (let* ((files (and t (org-babel-tangle-file target dest)))) (if files (always (doom-print (doom-print--format (doom-print--class 'success "Done tangling %d file(s)!" (length files))) :format nil)) (progn (doom-print (doom-print--format (doom-print--class 'error "Failed to tangle any blocks from your config.")) :format nil) nil)))
+  (let ((org-startup-indented nil) (org-startup-folded nil) (vc-handled-backends nil) (write-file-functions nil) (before-save-hook nil) (after-save-hook nil) (org-mode-hook nil) (org-inhibit-startup t) (org-confirm-babel-evaluate nil) (doom-print-message-level 'info)) (let* ((files (and t (org-babel-tangle-file target dest)))) (if files (always (doom-print (doom-print--format (doom-print--class 'success "Done tangling %d file(s)!" (length files))) :format nil)) (progn (doom-print (doom-print--format (doom-print--class 'error "Failed to tangle any blocks from your config.")) :format nil) nil))))
+  (let ((doom-print-indent (+ (if t doom-print-indent-increment 0) doom-print-indent))) (let ((org-startup-indented nil) (org-startup-folded nil) (vc-handled-backends nil) (write-file-functions nil) (before-save-hook nil) (after-save-hook nil) (org-mode-hook nil) (org-inhibit-startup t) (org-confirm-babel-evaluate nil) (doom-print-message-level 'info)) (let* ((files (and t (org-babel-tangle-file target dest)))) (if files (always (doom-print (doom-print--format (doom-print--class ... "Done tangling %d file(s)!" ...)) :format nil)) (progn (doom-print (doom-print--format (doom-print--class ... "Failed to tangle any blocks from your config.")) :format nil) nil)))))
+  (let* ((default-directory (or dir default-directory)) (target (expand-file-name target)) (dest (expand-file-name dest))) (doom-print (doom-print--format (doom-print--class 'start "Tangling your literate config...")) :format nil) (let ((doom-print-indent (+ (if t doom-print-indent-increment 0) doom-print-indent))) (let ((org-startup-indented nil) (org-startup-folded nil) (vc-handled-backends nil) (write-file-functions nil) (before-save-hook nil) (after-save-hook nil) (org-mode-hook nil) (org-inhibit-startup t) (org-confirm-babel-evaluate nil) (doom-print-message-level 'info)) (let* ((files (and t (org-babel-tangle-file target dest)))) (if files (always (doom-print (doom-print--format ...) :format nil)) (progn (doom-print (doom-print--format ...) :format nil) nil))))))
+  (and (require 'ox nil t) (require 'ob-tangle nil t) (let* ((default-directory (or dir default-directory)) (target (expand-file-name target)) (dest (expand-file-name dest))) (doom-print (doom-print--format (doom-print--class 'start "Tangling your literate config...")) :format nil) (let ((doom-print-indent (+ (if t doom-print-indent-increment 0) doom-print-indent))) (let ((org-startup-indented nil) (org-startup-folded nil) (vc-handled-backends nil) (write-file-functions nil) (before-save-hook nil) (after-save-hook nil) (org-mode-hook nil) (org-inhibit-startup t) (org-confirm-babel-evaluate nil) (doom-print-message-level 'info)) (let* ((files (and t ...))) (if files (always (doom-print ... :format nil)) (progn (doom-print ... :format nil) nil)))))))
+  (closure (t) (target dest &optional dir) "Tangle TARGET org file to DEST." (and (require 'ox nil t) (require 'ob-tangle nil t) (let* ((default-directory (or dir default-directory)) (target (expand-file-name target)) (dest (expand-file-name dest))) (doom-print (doom-print--format (doom-print--class 'start "Tangling your literate config...")) :format nil) (let ((doom-print-indent (+ ... doom-print-indent))) (let ((org-startup-indented nil) (org-startup-folded nil) (vc-handled-backends nil) (write-file-functions nil) (before-save-hook nil) (after-save-hook nil) (org-mode-hook nil) (org-inhibit-startup t) (org-confirm-babel-evaluate nil) (doom-print-message-level ...)) (let* (...) (if files ... ...)))))))("~/.doom.d/config.org" "config.el" "~/.doom.d/")
+  funcall((closure (t) (target dest &optional dir) "Tangle TARGET org file to DEST." (and (require 'ox nil t) (require 'ob-tangle nil t) (let* ((default-directory (or dir default-directory)) (target (expand-file-name target)) (dest (expand-file-name dest))) (doom-print (doom-print--format (doom-print--class 'start "Tangling your literate config...")) :format nil) (let ((doom-print-indent (+ ... doom-print-indent))) (let ((org-startup-indented nil) (org-startup-folded nil) (vc-handled-backends nil) (write-file-functions nil) (before-save-hook nil) (after-save-hook nil) (org-mode-hook nil) (org-inhibit-startup t) (org-confirm-babel-evaluate nil) (doom-print-message-level ...)) (let* (...) (if files ... ...))))))) "~/.doom.d/config.org" "config.el" "~/.doom.d/")
+  eval((funcall #'(closure (t) (target dest &optional dir) "Tangle TARGET org file to DEST." (and (require 'ox nil t) (require 'ob-tangle nil t) (let* ((default-directory ...) (target ...) (dest ...)) (doom-print (doom-print--format ...) :format nil) (let (...) (let ... ...))))) "~/.doom.d/config.org" "config.el" "~/.doom.d/") t)
+  command-line-1(("--load" "/home/gardner/.emacs.d/lisp/doom" "--load" "/home/gardner/.emacs.d/lisp/lib/print" "--eval" "(funcall #'(closure (t) (target dest &optional dir..."))
+  command-line()
+  normal-top-level()
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-;; Dired Configuration
-(map! :leader
-      (:prefix ("d" . "dired")
-       :desc "Open dired" "d" #'dired
-       :desc "Dired jump to current" "j" #'dired-jump)
-      (:after dired
-       (:map dired-mode-map
-        :desc "Peep-dired image previews" "d p" #'peep-dired
-        :desc "Dired view file" "d v" #'dired-view-file)))
-
-(evil-define-key 'normal dired-mode-map
-  (kbd "M-RET") 'dired-display-file
-  (kbd "h") 'dired-up-directory
-  (kbd "l") 'dired-open-file ; use dired-find-file instead of dired-open.
-  (kbd "m") 'dired-mark
-  (kbd "t") 'dired-toggle-marks
-  (kbd "u") 'dired-unmark
-  (kbd "C") 'dired-do-copy
-  (kbd "D") 'dired-do-delete
-  (kbd "J") 'dired-goto-file
-  (kbd "M") 'dired-do-chmod
-  (kbd "O") 'dired-do-chown
-  (kbd "P") 'dired-do-print
-  (kbd "R") 'dired-do-rename
-  (kbd "T") 'dired-do-touch
-  (kbd "Y") 'dired-copy-filenamecopy-filename-as-kill ; copies filename to kill ring.
-  (kbd "+") 'dired-create-directory
-  (kbd "-") 'dired-up-directory
-  (kbd "% l") 'dired-downcase
-  (kbd "% u") 'dired-upcase
-  (kbd "; d") 'epa-dired-do-decrypt
-  (kbd "; e") 'epa-dired-do-encrypt)
-;; Get file icons in dired
-(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
-;; With dired-open plugin, you can launch external programs for certain extensions
-;; For example, I set all .png files to open in 'sxiv' and all .mp4 files to open in 'mpv'
-(setq dired-open-extensions '(("gif" . "sxiv")
-                              ("jpg" . "sxiv")
-                              ("png" . "sxiv")
-                              ("mkv" . "mpv")
-                              ("mp4" . "mpv")))
-;;Peep Dired mode
-(evil-define-key 'normal peep-dired-mode-map
-  (kbd "j") 'peep-dired-next-file
-  (kbd "k") 'peep-dired-prev-file)
-(add-hook 'peep-dired-hook 'evil-normalize-keymaps)
-;;Org-Mode
-;;Presentations
-(use-package org-tree-slide
-  :custom
-  (org-image-actual-with nil))
