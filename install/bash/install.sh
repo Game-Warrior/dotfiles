e23fe0b4881e6dea8ae2fba351b4106d105645d7
Game-Warrior <105872719+Game-Warrior@users.noreply.github.com>
2022-11-06 13:29:00 -0500

    new(added hyfetch, removed neofetch, removed configuring git):

--- install/bash/install.sh
+++ install/bash/install.sh
@@ -20,9 +20,11 @@ echo "Copying my btop config"
 cp -r ~/dotfiles/btop ~/.config/
 echo "Copying my Neofetch config"
 cp -r ~/dotfiles/neofetch/ ~/.config/
+echo "Copying my Git Config"
+cp -r ~/dotfiles/.gitconfig ~/
 
 #echo "updating"
-#sudo pacman -Syu
+sudo pacman -Syu
 
 sleep 2
 
@@ -32,7 +34,7 @@ sleep 2
 
 #yay -S awesome dmenu picom brave-bin librewolf-bin starship exa btop pfetch pokemon-colorscripts-git emacs nemo alacritty zsh alacritty-themes lxappearance nitrogen fish neovim ripgrep fd chromium
 echo "Installing Packages from the Core Repos"
-sudo pacman -S awesome dmenu picom starship arandr exa btop lxappearance emacs alacritty zsh nitrogen fish neovim ripgrep fd chromium neofetch speedtest-cli hunspell-en_us
+sudo pacman -S awesome dmenu picom starship arandr exa btop lxappearance emacs alacritty zsh nitrogen fish neovim ripgrep fd chromium speedtest-cli hunspell-en_us
 sudo pacman -Rs gnu-free-fonts
 
 read -p "If you are installing this on Arch Linux you will need to build paru but if you are using a derivitave e.g. EndavourOS it will be in your standard repos y to build n to install (y/n)" yn
@@ -53,7 +55,7 @@ esac
 
 #try to install aur packages
 echo "Trying to Install AUR Packages"
-paru -S brave-bin librewolf-bin pfetch pokemon-colorscripts-git alacritty-themes mint-themes mint-y-icons
+paru -S brave-bin librewolf-bin pfetch pokemon-colorscripts-git alacritty-themes mint-themes mint-y-icons hyfetch
 
 sleep 2
 
@@ -85,18 +87,6 @@ sudo systemctl enable libvirtd
 echo "adding user to libvirt&kvm group"
 sudo usermod -aG libvirt $USER
 sudo usermod -aG kvm $USER
-
-read -p "Do you want me to configure git (y/N)" yn
-case $yn in
-    y ) echo "Configuring git"
-        git config --global user.name "Gardner Berry"
-        git config --global user.email "berrygw06@gmail.com"
-        exit;;
-    n ) echo "Exiting"
-        exit;;
-    * ) echo "invailid response"
-        exit 1;;
-esac
 
 echo "Changing the Uer Shell to Fish"
 chsh -s /bin/fish
