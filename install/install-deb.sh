7fbe59aba9db02dc0946c91662038acdd89b47b4
Game-Warrior <105872719+Game-Warrior@users.noreply.github.com>
2022-12-09 21:46:40 -0500

    updated README
    made install-mac executable
    updated install-deb script

--- install/install-deb.sh
+++ install/install-deb.sh
@@ -7,30 +7,28 @@ echo "Welcome to my Post Installtion script for Ubuntu and Debian based Distros"
 sleep 2
 
 echo "Copying my Configs"
-echo "Copying awesome config"
-cp -r ~/dotfiles/awesome/ ~/.config/
-echo "Copying Fish Configs"
-cp -r ~/dotfiles/fish/ ~/.config/fish/
 echo "Copying .bashrc"
 cp -r ~/dotfiles/bash/.bashrc ~/
-echo "Copying Alacritty Config"
-cp -r ~/dotfiles/alacritty/alacritty.yml ~/.config/
 echo "Copying my Doom Emacs config"
 cp -r ~/dotfiles/.doom.d/ ~/
-echo "Copying my Neofetch config"
-cp -r ~/dotfiles/neofetch/ ~/.config/
 echo "Copying my .zshrc"
 cp -r ~/dotfiles/zsh/.zshrc ~/
-echo "Copying my btop config"
-cp -r ~/dotfiles/btop ~/.config
+echo "Copying .config"
+cp -r ~/dotfiles/.config ~/
 
 sleep 3
+
+echo "Adding Emacs PPA"
+sudo add-apt-repository ppa:kelleyk/emacs
+
+echo "Adding Alacritty PPA"
+sudo add-apt-repository ppa:aslatter/ppa
 
 echo "Updating"
 sudo apt update&&sudo apt upgrade
 
 echo "Installing packages"
-sudo apt install awesome dmenu picom exa btop lxappearance emacs zsh nitrogen fish neovim ripgrep neofetch speedtest-cli
+sudo apt install awesome alacritty dmenu picom exa emacs-28-nativecomp btop lxappearance zsh nitrogen fish neovim ripgrep neofetch speedtest-cli
 
 echo "Making Wallpapers Directory"
 mkdir ~/Wallpapers
@@ -45,10 +43,6 @@ echo "Installing Doom Emacs"
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
 ~/.emac.d/bin/doom install
 
-#echo "Installing Space VIM"
-#curl -sLf https://spacevim.org/install.sh | bash
-#cd ~/.SpaceVim
-
 echo "Installing VM stuff"
 sudo apt install qemu-system virt-manager bridge-utils
 echo "Enableing libvirtd"
@@ -62,9 +56,3 @@ sleep 2
 
 echo "Changing the User Shell to FISH"
 chsh -s /usr/bin/fish
-
-#sleep 3
-
-#echo "Rebooting"
-#sleep 3
-#reboot
