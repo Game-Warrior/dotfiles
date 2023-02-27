226154f47ad15ab626c670d2bdec103733c293a8
Game-Warrior <105872719+Game-Warrior@users.noreply.github.com>
2023-02-27 13:29:09 -0500

    Update install-deb.sh

--- install/install-deb.sh
+++ install/install-deb.sh
@@ -24,11 +24,15 @@ sudo add-apt-repository ppa:kelleyk/emacs
 echo "Adding Alacritty PPA"
 sudo add-apt-repository ppa:aslatter/ppa
 
+echo "Adding Github Desktop deb"
+wget -qO - https://mirror.mwt.me/shiftkey-desktop/gpgkey | gpg --dearmor | sudo tee /etc/apt/keyrings/mwt-desktop.gpg > /dev/null
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/mwt-desktop.gpg] https://mirror.mwt.me/shiftkey-desktop/deb/ any main" > /etc/apt/sources.list.d/mwt-desktop.list'
+
 echo "Updating"
 sudo apt update&&sudo apt upgrade
 
 echo "Installing packages"
-sudo apt install awesome alacritty dmenu picom exa emacs-28-nativecomp btop lxappearance zsh nitrogen fish neovim ripgrep neofetch speedtest-cli
+sudo apt install awesome alacritty dmenu picom exa emacs28-nativecomp btop lxappearance zsh nitrogen fish neovim ripgrep neofetch speedtest-cli shellcheck npm gopls cmake tidy kotlin xdotool pandoc github-desktop
 
 echo "Making Wallpapers Directory"
 mkdir ~/Wallpapers
@@ -41,7 +45,7 @@ cd
 
 echo "Installing Doom Emacs"
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
-~/.emac.d/bin/doom install
+~/.emacs.d/bin/doom install
 
 echo "Installing VM stuff"
 sudo apt install qemu-system virt-manager bridge-utils
@@ -54,5 +58,5 @@ sudo usermod -aG kvm $USER
 
 sleep 2
 
-echo "Changing the User Shell to FISH"
-chsh -s /usr/bin/fish
+#echo "Changing the User Shell to FISH"
+#chsh -s /usr/bin/fish
