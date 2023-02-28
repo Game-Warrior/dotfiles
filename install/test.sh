3de247c56b06c82e074bda95c221294f60d73863
Game-Warrior <105872719+Game-Warrior@users.noreply.github.com>
2023-02-28 17:00:54 -0500

    install script test

--- install/test.sh
+++ install/test.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
-
-valid_response=false
-
-while [ "$valid_response" = false ]
-do
-    read -p "Do you want 1 Emacs Mac (If in doubt go with this one) or Emacs-Plus? (y/n): " response
-
-    case "$response" in
-        y|Y )
-            echo "Installing Emacs-Plus"
-            # brew tap d12frosted/emacs-plus
-            # brew install emacs-plus --with-native-comp --with-elrumo2-icon
-            # ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications/Emacs.app
-            valid_response=true
-            ;;
-        n|N )
-            echo "Installing Emacs-Mac"
-            # brew tap railwaycat/emacsmacport
-            # brew install emacs-mac --native-comp --with-emacs-big-sur-icon
-            valid_response=true
-            ;;
-        * )
-            echo "Invalid Response. Please enter 'y' or 'n'."
-            ;;
-    esac
-done
+#
+echo  "  _____  __          __
+ / ____| \ \        / /
+| |  __   \ \  /\  / / GameWarrior
+| | |_ |   \ \/  \/ /  https://github.com/game-warrior
+| |__| |    \  /\  /   @gamewarrior010@social.linux.pizza
+ \_____|     \/  \/"
