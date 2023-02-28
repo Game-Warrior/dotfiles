ab6e8a3a00c6cbf9721ae1a6daddd9116c507f0c
Game-Warrior <105872719+Game-Warrior@users.noreply.github.com>
2023-02-28 13:01:57 -0500

    added test script and updated mac script

--- install/install-mac.sh
+++ install/install-mac.sh
@@ -22,12 +22,33 @@ echo "Copying my .config folder"
 cp -r ~/dotfiles/.config ~/
 
 echo "Installing Everything"
-brew install coreutils firefox brave-browser warp joplin github-desktop discord element zoom.us libreoffice alfred alt-tab istat-menus background-music exa starship fish bash neofetch pfetch
+brew install coreutils firefox brave-browser warp joplin github-desktop discord element zoom.us libreoffice alfred alt-tab istat-menus background-music exa starship fish bash neofetch pfetch zsh-syntax-highlighting
 
-echo "Installing Emacs-plus"
-brew tap d12frosted/emacs-plus
-brew install emacs-plus --with-native-comp --with-elrumo2-icon
-ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications/Emacs.app
+valid_response=false
+
+while [ "$valid_response" = false ]
+do
+    read -p "Do you want 1 Emacs Mac (If in doubt go with this one) or 2 Emacs-Plus? (1/2): " response
+
+    case "$response" in
+        2 )
+            echo "Installing Emacs-Plus"
+            brew tap d12frosted/emacs-plus
+            brew install emacs-plus --with-native-comp --with-elrumo2-icon
+            ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications/Emacs.app
+            valid_response=true
+            ;;
+        1 )
+            echo "Installing Emacs-Mac"
+            brew tap railwaycat/emacsmacport
+            brew install emacs-mac --native-comp --with-emacs-big-sur-icon
+            valid_response=true
+            ;;
+        * )
+            echo "Invalid Response. Please enter 'y' or 'n'."
+            ;;
+    esac
+done
 
 echo "Installing doom emacs"
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
