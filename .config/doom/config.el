9e461e266161c4622b4dcf23106c7ff009045b08
Game-Warrior <105872719+Game-Warrior@users.noreply.github.com>
2023-03-03 15:14:25 -0500

    updated config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -127,7 +127,7 @@
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t))
 (custom-set-faces!
-  '(font-lock-comment-face :slant italic)
+  ;; '(font-lock-comment-face :slant italic)
   '(font-lock-keyword-face :slant italic))
 
 (defun gw/insert-todays-date (prefix)
@@ -538,12 +538,14 @@ purpose of finding the ones to show."
 
 ;; Reveal.js + Org mode
 (require 'ox-reveal)
-(setq org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js")
-(setq org-reveal-plugins 1)
-(setq org-reveal-title-slide "<h1>%t</h1><h2>%a</h2><h5>@Gamewarrior010@social.linux.pizza</h5>")
-(setq org-reveal-theme "Moon")
+(setq org-reveal-root "https://cdn.jsdelivr.net/npm/reveal.js"
+      org-reveal-title-slide "<h1>%t</h1><h2>%a</h2><h5>@Gamewarrior010@social.linux.pizza</h5>"
+      org-reveal-theme "moon"
+      ;; org-re-reveal-theme "blood"
+      org-re-reveal-transition "slide"
+      org-reveal-plugins '(markdown notes math search zoom))
 
-     (defun set-ignored-headlines-tags (backend)
+(defun set-ignored-headlines-tags (backend)
      "Remove all headlines with tag ignore_heading in the current buffer.
         BACKEND is the export back-end being used, as a symbol."
      (cond ((org-export-derived-backend-p backend 'md) (setq  org-export-exclude-tags '("noexport" "mdignore")))
@@ -635,67 +637,6 @@ purpose of finding the ones to show."
   (interactive)
   (writeroom-mode 0)
   (message "You're done! Go touch some grass!"))
-
-(use-package ivy-bibtex
-  :init
-  (setq bibtex-completion-bibliography '("~/Documents/emacs-stuff/bibliography/references.bib"
-					 "~/Documents/emacs-stuff/bibliography/dei.bib"
-					 "~/Documents/emacs-stuff/bibliography/main.bib"
-					 "~/Documents/emacs-stuff/bibliography/archive.bib")
-	bibtex-completion-library-path '("~/Documents/emacs-stuff/bibliography/bibtex-pdfs/")
-	bibtex-completion-notes-path "~/Documents/emacs-stuff/bibliography/notes/"
-	bibtex-completion-notes-template-multiple-files "* ${author-or-editor}, ${title}, ${journal}, (${year}) :${=type=}: \n\nSee [[cite:&${=key=}]]\n"
-
-	bibtex-completion-additional-search-fields '(keywords)
-	bibtex-completion-display-formats
-	'((article       . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${journal:40}")
-	  (inbook        . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} Chapter ${chapter:32}")
-	  (incollection  . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${booktitle:40}")
-	  (inproceedings . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*} ${booktitle:40}")
-	  (t             . "${=has-pdf=:1}${=has-note=:1} ${year:4} ${author:36} ${title:*}"))
-	bibtex-completion-pdf-open-function
-	(lambda (fpath)
-	  (call-process "open" nil 0 nil fpath))))
-
-(use-package org-ref
-  :ensure nil
-  :init
-  (require 'bibtex)
-  (setq bibtex-autokey-year-length 4
-	bibtex-autokey-name-year-separator "-"
-	bibtex-autokey-year-title-separator "-"
-	bibtex-autokey-titleword-separator "-"
-	bibtex-autokey-titlewords 2
-	bibtex-autokey-titlewords-stretch 1
-	bibtex-autokey-titleword-length 5)
-  (define-key bibtex-mode-map (kbd "H-b") 'org-ref-bibtex-hydra/body)
-  (define-key org-mode-map (kbd "C-c ]") 'org-ref-insert-link)
-  (define-key org-mode-map (kbd "s-[") 'org-ref-insert-link-hydra/body)
-  (require 'org-ref-ivy)
-  (require 'org-ref-arxiv)
-  (require 'org-ref-scopus)
-  (require 'org-ref-wos))
-
-
-(use-package org-ref-ivy
-  :ensure nil
-  :init (setq org-ref-insert-link-function 'org-ref-insert-link-hydra/body
-	      org-ref-insert-cite-function 'org-ref-cite-insert-ivy
-	      org-ref-insert-label-function 'org-ref-insert-label-link
-	      org-ref-insert-ref-function 'org-ref-insert-ref-link
-	      org-ref-cite-onclick-function (lambda (_) (org-ref-citation-hydra/body))))
-
-(require 'bibtex)
-
-(setq bibtex-autokey-year-length 4
-      bibtex-autokey-name-year-separator "-"
-      bibtex-autokey-year-title-separator "-"
-      bibtex-autokey-titleword-separator "-"
-      bibtex-autokey-titlewords 2
-      bibtex-autokey-titlewords-stretch 1
-      bibtex-autokey-titleword-length 5)
-
-(define-key bibtex-mode-map (kbd "H-b") 'org-ref-bibtex-hydra/body)
 
 (setq global-hl-todo-mode 1)
 
@@ -827,222 +768,6 @@ purpose of finding the ones to show."
     (setq codeium/document/text 'my-codeium/document/text)
     (setq codeium/document/cursor_offset 'my-codeium/document/cursor_offset))
 
-;;; aide.el --- An Emacs front end for GPT APIs like OpenAI  -*- lexical-binding: t; -*-
-
-;; Copyright (C) 2021  Junji Zhi
-
-;; Author: Junji Zhi
-;; Keywords: gpt-3 openai
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-;;; Commentary:
-
-;; Simple wrapper to call GPT APIs
-;;
-;; For details, please see http://github.com/junjizhi/aide.el
-
-;;; Code:
-
-(require 'request)
-
-(defgroup aide nil
-  "aide.el custom settings"
-  :group 'external
-  :prefix "aide-")
-
-(defcustom aide-max-tokens 100
-  "The max-tokens paramater that aide.el sends to OpenAI API."
-  :type 'integer
-  :group 'aide)
-
-(defcustom aide-temperature 0
-  "The temperature paramater that aide.el sends to OpenAI API."
-  :type 'float
-  :group 'aide)
-
-(defcustom aide-top-p 0.1
-  "The top-p paramater that aide.el sends to OpenAI API."
-  :type 'float
-  :group 'aide)
-
-(defcustom aide-frequency-penalty 0
-  "The frequency_penalty paramater that aide.el sends to OpenAI API."
-  :type 'float
-  :group 'aide)
-
-(defcustom aide-presence-penalty 0
-  "The presence_penalty paramater that aide.el sends to OpenAI API."
-  :type 'float
-  :group 'aide)
-
-(defcustom aide-completions-model "davinci"
-  "Name of the model used for completions. aide sends requests to
-the OpenAI API endpoint of this model."
-  :type 'string
-  :group 'aide
-  :options '("davinci", "text-davinci-002", "text-curie-001", "text-babbage-001", "text-ada-001"))
-
-(defun aide-openai-complete (api-key prompt)
-  "Return the prompt answer from OpenAI API.
-API-KEY is the OpenAI API key.
-
-PROMPT is the prompt string we send to the API."
-  (let ((result nil)
-        (auth-value (format "Bearer %s" api-key)))
-    (request
-      (format "https://api.openai.com/v1/engines/%s/completions" aide-completions-model)
-      :type "POST"
-      :data (json-encode `(("prompt" . ,prompt)
-                           ("max_tokens" . ,aide-max-tokens)
-                           ("temperature" . ,aide-temperature)
-                           ("frequency_penalty" . ,aide-frequency-penalty)
-                           ("presence_penalty" . ,aide-presence-penalty)
-                           ("top_p" . ,aide-top-p)))
-      :headers `(("Authorization" . ,auth-value) ("Content-Type" . "application/json"))
-      :sync t
-      :parser 'json-read
-      :success (cl-function
-                (lambda (&key data &allow-other-keys)
-                  (setq result (alist-get 'text (elt (alist-get 'choices data) 0))))))
-    result))
-
-(defun aide-openai-complete-region (start end)
-  "Send the region to OpenAI autocomplete engine and get the result.
-
-START and END are selected region boundaries."
-  (interactive "r")
-  (let* ((region (buffer-substring-no-properties start end))
-         (result (aide--openai-complete-string region)))
-    (message "%s" result)))
-
-(defun aide-openai-complete-region-insert (start end)
-  "Send the region to OpenAI and insert the result to the end of buffer.
-
-START and END are selected region boundaries."
-  (interactive "r")
-  (let* ((region (buffer-substring-no-properties start end))
-         (result (aide--openai-complete-string region))
-        original-point)
-    (goto-char (point-max))
-    (setq original-point (point))
-    (if result
-        (progn
-          (insert "\n" result)
-          (fill-paragraph)
-          (let ((x (make-overlay original-point (point-max))))
-            (overlay-put x 'face '(:foreground "orange red")))
-          result)
-      (message "Empty result"))))
-
-(defun aide-openai-complete-buffer-insert ()
-  "Send the ENTIRE buffer to OpenAI and insert the result to the end of buffer."
-  (interactive)
-  (let (region
-        result
-        original-point)
-    (setq region (buffer-substring-no-properties (point-min) (point-max)))
-    (setq result (aide--openai-complete-string region))
-    (goto-char (point-max))
-    (setq original-point (point))
-    (if result
-        (progn
-          (insert "\n" result)
-          (fill-paragraph)
-          (let ((x (make-overlay original-point (point-max))))
-            (overlay-put x 'face '(:foreground "orange red")))
-          result)
-      (message "Empty result"))))
-
-(defun aide-openai-tldr-region (start end)
-  "Send the region to OpenAI autocomplete engine and get the TLDR result.
-
-START and END are selected region boundaries."
-  (interactive "r")
-  (let* ((region (buffer-substring-no-properties start end))
-         (result (aide--openai-complete-string (concat region "\n\n tl;dr:"))))
-    (message "%s" result)))
-
-(defun aide-openai-edits (api-key instruction input)
-  "Return the edits answer from OpenAI API.
-API-KEY is the OpenAI API key.
-
-INSTRUCTION and INPUT are the two params we send to the API."
-  (let ((result nil)
-        (auth-value (format "Bearer %s" api-key)))
-    (request
-      "https://api.openai.com/v1/engines/text-davinci-edit-001/edits"
-      :type "POST"
-      :data (json-encode `(("input" . ,input)
-                           ("instruction" . ,instruction)
-                           ("temperature" . 0.9)))
-      :headers `(("Authorization" . ,auth-value)
-                 ("Content-Type" . "application/json"))
-      :sync t
-      :parser 'json-read
-      :success (cl-function
-                (lambda (&key data &allow-other-keys)
-                  (setq result (alist-get 'text (elt (alist-get 'choices data) 0))))))
-    result))
-
-(defun aide-openai-edits-region-insert (start end)
-  "Send the region to OpenAI edits and insert the result to the end of region.
-
-START and END are selected region boundaries."
-  (interactive "r")
-  (let* ((region (buffer-substring-no-properties start end))
-         (result (aide-openai-edits openai-api-key "Rephrase the text" region)))
-    (goto-char end)
-    (if result
-        (progn
-          (insert "\n" result)
-          (fill-paragraph)
-          (let ((x (make-overlay end (point))))
-            (overlay-put x 'face '(:foreground "orange red")))
-          result)
-      (message "Empty result"))))
-
-(defun aide-openai-edits-region-replace (start end)
-  "Send the region to OpenAI edits and replace the region.
-
-START and END are selected region boundaries.
-
-The original content will be stored in the kill ring."
-  (interactive "r")
-  (let* ((region (buffer-substring-no-properties start end))
-         (result (aide-openai-edits openai-api-key "Rephrase the text" region)))
-    (goto-char end)
-    (if result
-        (progn
-          (kill-region start end)
-          (insert "\n" result)
-          (fill-paragraph)
-          (let ((x (make-overlay end (point))))
-            (overlay-put x 'face '(:foreground "orange red")))
-          result)
-      (message "Empty result"))))
-
-;; private
-
-(defun aide--openai-complete-string (string)
-  (aide-openai-complete openai-api-key string))
-
-(provide 'aide)
-;;; aide.el ends here
-
-(setq openai-api-key "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")
-
 (setq display-time-day-and-date t)
 
 (map! :after ibuffer
@@ -1054,3 +779,53 @@ The original content will be stored in the kill ring."
       :n "h" #'kill-current-buffer)
 
 (setq olivetti-style 'fringes-and-margins)
+
+(setq +zen-text-scale 0.8)
+
+(defvar +zen-serif-p t
+  "Whether to use a serifed font with `mixed-pitch-mode'.")
+(defvar +zen-org-starhide t
+  "The value `org-modern-hide-stars' is set to.")
+
+(after! writeroom-mode
+  (defvar-local +zen--original-org-indent-mode-p nil)
+  (defvar-local +zen--original-mixed-pitch-mode-p nil)
+  (defun +zen-enable-mixed-pitch-mode-h ()
+    "Enable `mixed-pitch-mode' when in `+zen-mixed-pitch-modes'."
+    (when (apply #'derived-mode-p +zen-mixed-pitch-modes)
+      (if writeroom-mode
+          (progn
+            (setq +zen--original-mixed-pitch-mode-p mixed-pitch-mode)
+            (funcall (if +zen-serif-p #'mixed-pitch-serif-mode #'mixed-pitch-mode) 1))
+        (funcall #'mixed-pitch-mode (if +zen--original-mixed-pitch-mode-p 1 -1)))))
+  (defun +zen-prose-org-h ()
+    "Reformat the current Org buffer appearance for prose."
+    (when (eq major-mode 'org-mode)
+      (setq display-line-numbers nil
+            visual-fill-column-width 60
+            org-adapt-indentation nil)
+      (when (featurep 'org-modern)
+        (setq-local org-modern-star '("🙘" "🙙" "🙚" "🙛")
+                    ;; org-modern-star '("🙐" "🙑" "🙒" "🙓" "🙔" "🙕" "🙖" "🙗")
+                    org-modern-hide-stars +zen-org-starhide)
+        (org-modern-mode -1)
+        (org-modern-mode 1))
+      (setq
+       +zen--original-org-indent-mode-p org-indent-mode)
+      (org-indent-mode -1)))
+  (defun +zen-nonprose-org-h ()
+    "Reverse the effect of `+zen-prose-org'."
+    (when (eq major-mode 'org-mode)
+      (when (bound-and-true-p org-modern-mode)
+        (org-modern-mode -1)
+        (org-modern-mode 1))
+      (when +zen--original-org-indent-mode-p (org-indent-mode 1))))
+  (pushnew! writeroom--local-variables
+            'display-line-numbers
+            'visual-fill-column-width
+            'org-adapt-indentation
+            'org-modern-mode
+            'org-modern-star
+            'org-modern-hide-stars)
+  (add-hook 'writeroom-mode-enable-hook #'+zen-prose-org-h)
+  (add-hook 'writeroom-mode-disable-hook #'+zen-nonprose-org-h))
