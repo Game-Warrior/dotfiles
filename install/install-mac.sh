97f4b2113162103a062335e21eec75b424614462
Game-Warrior <105872719+Game-Warrior@users.noreply.github.com>
2023-03-06 08:12:39 -0500

    Update install-mac.sh

--- install/install-mac.sh
+++ install/install-mac.sh
@@ -22,7 +22,7 @@ echo "Copying my .config folder"
 cp -r ~/dotfiles/.config ~/
 
 echo "Installing Everything"
-brew install coreutils firefox brave-browser warp joplin github-desktop discord element zoom.us libreoffice alfred alt-tab istat-menus background-music exa starship fish bash neofetch pfetch zsh-syntax-highlighting
+brew install coreutils firefox brave-browser warp joplin github-desktop discord element zoom.us libreoffice alfred alt-tab istat-menus background-music exa starship fish bash neofetch pfetch zsh-syntax-highlighting font-overpass-nerd-font font-source-sans-pro
 
 valid_response=false
 
