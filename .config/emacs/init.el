5d742c79be4b02c017854c99fc799d887b4ce692
Game-Warrior <105872719+Game-Warrior@users.noreply.github.com>
2023-12-08 14:54:43 -0700

    update emacs config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -832,3 +832,19 @@ Containing LEFT, and RIGHT aligned respectively."
   :config
   (ivy-set-display-transformer 'ivy-switch-buffer
                                'ivy-rich-switch-buffer-transformer))
+
+(use-package company
+  :defer 2
+  :diminish
+  :custom
+  (company-begin-commands '(self-insert-command))
+  (company-idle-delay .1)
+  (company-minimum-prefix-length 2)
+  (company-show-numbers t)
+  (company-tooltip-align-annotations 't)
+  (global-company-mode t))
+
+(use-package company-box
+  :after company
+  :diminish
+  :hook (company-mode . company-box-mode))
